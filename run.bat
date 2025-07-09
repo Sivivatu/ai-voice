26f4ff353c71bb9e9b875d0517f69f9741eec546
Paul <Sivivatu@users.noreply.github.com>
2025-07-09 18:55:56 +0100

    chore: Update run.bat to use pnpm for package installations

--- run.bat
+++ run.bat
@@ -5,9 +5,9 @@ call pnpx expo install expo-dev-client
 call pnpx expo install posthog-react-native expo-file-system expo-application expo-device expo-localization
 @REM pnpx expo install posthog-reaact-native-session-replay
 
-call pnpx i openai
+call pnpm i openai
 
-call pnpx expo install sonner-native
-call pnpx expo install react-native-svg
+call pnpm expo install sonner-native
+call pnpm expo install react-native-svg
 
-call pnpx expo prebuild
+call pnpm expo prebuild
