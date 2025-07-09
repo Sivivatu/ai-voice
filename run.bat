645a5a7951baecb2119f824ff486c2c2b87b17f5
Paul <Sivivatu@users.noreply.github.com>
2025-07-09 13:41:50 +0100

    setup: Add run.bat script to install necessary Expo packages

---
+++ run.bat
@@ -0,0 +1,13 @@
+call pnpx expo install firebase
+call pnpx expo install expo-audio
+call pnpx expo install expo-dev-client
+
+call pnpx expo install posthog-react-native expo-file-system expo-application expo-device expo-localization
+@REM pnpx expo install posthog-reaact-native-session-replay
+
+call pnpx i openai
+
+call pnpx expo install sonner-native
+call pnpx expo install react-native-svg
+
+call pnpx expo prebuild
